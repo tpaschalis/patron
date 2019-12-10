ea43dd565f971ce45f590ab55cdc7c015ba75ed9
Paschalis Tsilias <p.tsilias@thebeat.co>
2019-12-10 10:09:02 +0200

    Add benchark function, second test case

--- trace/sql/sql_test.go
+++ trace/sql/sql_test.go
@@ -13,10 +13,17 @@ func TestParseDSN(t *testing.T) {
 	}
 	var testcases = []testcase{
 		{"username:password@protocol(address)/dbname?param=value", DSNData{"dbname", "address", "username", "protocol", "password"}, nil},
+		{"bruce:hunter2@tcp(127.0.0.1)/arkhamdb?param=value", DSNData{"arkhamdb", "127.0.0.1", "bruce", "tcp", "hunter2"}, nil},
 	}
 
 	for _, tc := range testcases {
 		got, _ := ParseDSN(tc.dsn)
 		assert.Equal(t, got, tc.want)
+	}
+}
+
+func BenchmarkParseDSN(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ParseDSN("bruce:hunter2@tcp(127.0.0.1)/arkhamdb?param=value")
 	}
 }
