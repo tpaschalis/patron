fce47daf68212245e759027f12e9a89962b2eb2f
Paschalis Tsilias <p.tsilias@thebeat.co>
2019-12-11 09:40:34 +0200

    Add more testcases; Don't forget we have to refactor these tests

--- trace/sql/sql_test.go
+++ trace/sql/sql_test.go
@@ -7,17 +7,27 @@ import (
 
 func TestParseDSN(t *testing.T) {
 	type testcase struct {
-		dsn     string
-		want    DSNData
-		wantErr error
+		dsn  string
+		want DSNInfo
 	}
+
+	//TODO: restructure table-driven tests to the proposed way
 	var testcases = []testcase{
-		{"username:password@protocol(address)/dbname?param=value", DSNData{"dbname", "address", "username", "protocol", "password"}, nil},
-		{"bruce:hunter2@tcp(127.0.0.1)/arkhamdb?param=value", DSNData{"arkhamdb", "127.0.0.1", "bruce", "tcp", "hunter2"}, nil},
+		{"username:password@protocol(address)/dbname?param=value", DSNInfo{"dbname", "address", "username", "protocol", "password"}},
+		{"/", DSNInfo{"", "", "", "", ""}},
+		{"/dbname", DSNInfo{"dbname", "", "", "", ""}},
+		{"user:p@/ssword@/", DSNInfo{"", "", "user", "", "p@/ssword"}},
+		{"user@unix(/path/to/socket)/dbname?charset=utf8", DSNInfo{"dbname", "/path/to/socket", "user", "unix", ""}},
+		{"user:password@/dbname?param1=val1&param2=val2&param3=val3", DSNInfo{"dbname", "", "user", "", "password"}},
+		{"bruce:hunter2@tcp(127.0.0.1)/arkhamdb?param=value", DSNInfo{"arkhamdb", "127.0.0.1", "bruce", "tcp", "hunter2"}},
+		{"user@unix(/path/to/mydir@/socket)/dbname?charset=utf8", DSNInfo{"dbname", "/path/to/mydir@/socket", "user", "unix", ""}},
+		{"user:password@tcp(localhost:5555)/dbname?charset=utf8&tls=true", DSNInfo{"dbname", "localhost:5555", "user", "tcp", "password"}},
+		{"us:er:name:password@memory(localhost:5555)/dbname?charset=utf8&tls=true", DSNInfo{"dbname", "localhost:5555", "us", "memory", "er:name:password"}},
+		{"user:p@ss(word)@tcp([c023:9350:225b:671a:2cdd:3d83:7c19:ca42]:80)/dbname?loc=Local", DSNInfo{"dbname", "[c023:9350:225b:671a:2cdd:3d83:7c19:ca42]:80", "user", "tcp", "p@ss(word)"}},
 	}
 
 	for _, tc := range testcases {
-		got, _ := ParseDSN(tc.dsn)
+		got := ParseDSN(tc.dsn)
 		assert.Equal(t, got, tc.want)
 	}
 }
