2c4d8116bf1064f0ba818e4d4b2dfaeedb252873
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-01-21 10:40:08 +0200

    Break lru_test into sub-tests.
    Signed-off-by: Paschalis Tsilias <p.tsilias@thebeat.co>

--- cache/lru/lru_test.go
+++ cache/lru/lru_test.go
@@ -2,7 +2,6 @@ package lru
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -24,48 +23,43 @@ func TestCacheOperations(t *testing.T) {
 	assert.NoError(t, err)
 
 	k, v := "foo", "bar"
-	res, exists, err := c.Get(k)
-	assert.Nil(t, res)
-	assert.False(t, exists)
-	assert.NoError(t, err)
 
-	err = c.Set(k, v)
-	assert.NoError(t, err)
+	t.Run("testGetEmpty", func(t *testing.T) {
+		res, ok, err := c.Get(k)
+		assert.Nil(t, res)
+		assert.False(t, ok)
+		assert.NoError(t, err)
+	})
 
-	res, exists, err = c.Get(k)
-	assert.Equal(t, v, res)
-	assert.True(t, exists)
-	assert.NoError(t, err)
+	t.Run("testSetGet", func(t *testing.T) {
+		err = c.Set(k, v)
+		assert.NoError(t, err)
+		res, ok, err := c.Get(k)
+		assert.Equal(t, v, res)
+		assert.True(t, ok)
+		assert.NoError(t, err)
+	})
 
-	err = c.Remove(k)
-	assert.NoError(t, err)
-	res, exists, err = c.Get(k)
-	assert.Nil(t, res)
-	assert.False(t, exists)
-	assert.NoError(t, err)
+	t.Run("testRemove", func(t *testing.T) {
+		err = c.Remove(k)
+		assert.NoError(t, err)
+		res, ok, err := c.Get(k)
+		assert.Nil(t, res)
+		assert.False(t, ok)
+		assert.NoError(t, err)
+	})
 
-	err = c.Set("key1", "val1")
-	assert.NoError(t, err)
-	err = c.Set("key2", "val2")
-	assert.NoError(t, err)
-	err = c.Set("key3", "val3")
-	assert.NoError(t, err)
+	t.Run("testPurge", func(t *testing.T) {
+		err = c.Set("key1", "val1")
+		assert.NoError(t, err)
+		err = c.Set("key2", "val2")
+		assert.NoError(t, err)
+		err = c.Set("key3", "val3")
+		assert.NoError(t, err)
 
-	assert.Equal(t, c.lru.Len(), 3)
-	err = c.Purge()
-	assert.NoError(t, err)
-	assert.Equal(t, c.lru.Len(), 0)
-
-	err = c.SetTTL(k, v, 500*time.Millisecond)
-	assert.NoError(t, err)
-	res, exists, err = c.Get(k)
-	assert.Equal(t, v, res)
-	assert.True(t, exists)
-	assert.NoError(t, err)
-
-	time.Sleep(600 * time.Millisecond)
-	res, exists, err = c.Get(k)
-	assert.Nil(t, res)
-	assert.False(t, exists)
-	assert.NoError(t, err)
+		assert.Equal(t, c.lru.Len(), 3)
+		err = c.Purge()
+		assert.NoError(t, err)
+		assert.Equal(t, c.lru.Len(), 0)
+	})
 }
