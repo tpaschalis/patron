bd290174c4d87bd506eeb0c409a0e20387f12daa
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-03-10 18:49:01 +0200

    Add trace/amqp integration tests as well
    Signed-off-by: Paschalis Tsilias <p.tsilias@thebeat.co>

---
+++ trace/amqp/amqp_integration_test.go
@@ -0,0 +1,60 @@
+// !build integration
+
+package amqp
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPublisherSuccess(t *testing.T) {
+	ctx := context.Background()
+	pub, err := NewPublisher("amqp://guest:guest@localhost:5672/", "exchangeName")
+	assert.NoError(t, err)
+
+	msg, err := NewJSONMessage(`{"broker":"🐰"}`)
+	assert.NoError(t, err)
+
+	err = pub.Publish(ctx, msg)
+	assert.NoError(t, err)
+
+	err = pub.Close(ctx)
+	assert.NoError(t, err)
+}
+
+func TestPublisherFailures(t *testing.T) {
+	type args struct {
+		url          string
+		exchangeName string
+		publisherErr string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"missing url", args{"", "", "url is required"}},
+		{"missing exchange", args{"foo", "", "exchange is required"}},
+		{"incorrect URL", args{"foo", "bar", "failed to open RabbitMq connection: AMQP scheme must be either 'amqp://' or 'amqps://'"}},
+		{"incorrect exchange", args{"amqp://guest:guest@localhost:5672/", "\n", "failed to declare exchange: Exception (403) Reason: \"ACCESS_REFUSED - operation not permitted on the default exchange\""}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewPublisher(tt.args.url, tt.args.exchangeName)
+			assert.EqualError(t, err, tt.args.publisherErr)
+
+		})
+	}
+}
+
+func TestPublishIntoClosedChannel(t *testing.T) {
+	ctx := context.Background()
+	pub, _ := NewPublisher("amqp://guest:guest@localhost:5672/", "foo")
+	msg, _ := NewJSONMessage(`"foo": "bar"`)
+
+	pub.ch.Close()
+	err := pub.Publish(ctx, msg)
+	assert.EqualError(t, err, "failed to publish message: Exception (504) Reason: \"channel/connection is not open\"")
+}
